d294519e6d1e8c09b712b8620a453a967d7c1f54
Keeyoshi <keeyoshi99@gmail.com>
2020-02-15 19:56:10 +0545

    Submition 1 Assigment

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-const http=require('http');
-const app=require('./app');
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
 
-const port=process.env.PORT ||3000;
+require('dotenv').config();
 
-const server=http.createServer(app);
+const app=express();
+const port=process.env.PORT || 5000;
 
-server.listen(port);
+app.use(cors());
+app.use(express.json());
+
+const uri=process.env.ATLAST_URI;
+mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true });
+
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log("Mongoose Database Connection Established Successfully");
+});
+
+const exercisesRouter=require('./routes/exercise');
+const usersRouter=require('./routes/users');
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
+
+app.listen(port,()=>{
+console.log(`Server is running on port: ${port}`);
+});
